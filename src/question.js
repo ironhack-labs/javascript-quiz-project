6e469b85cae5071fe963523119250a147b0de117
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-15 16:31:21 +0100

    first commit

--- src/question.js
+++ src/question.js
@@ -1,7 +1,15 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
 }
+
+
+class Question
+
+should receive 4 arguments in the constructor ).
+should have 4 properties: text, choices, answer, difficulty.
+should receive text (string) as its 1st argument and assign it to text property.
+should receive choices (array of strings) as its 2nd argument and assign it to choices property.
+should receive answer (string) as its 3rd argument and assign it to answer property.
+should receive difficulty (number) as its 3rd argument and assign it to difficulty property.
