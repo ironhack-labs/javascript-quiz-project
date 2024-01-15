340a7c5df37c4e1a54cc695e6aac07b1add97017
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-01-15 17:33:42 +0100

    almost done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text; // should be a string
+    this.choices = choices; // array of strings [1,2,3,4]
+    this.answer = answer; // string
+    this.difficulty = difficulty; // number between 1 and 3 (1 being the easiest)
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    let currentArray = this.choices; //spread operator would be better!
+    let shuffledArray = [currentArray[0]];
+
+    for (let i = 0; i < currentArray.length; i++) {
+      if (!shuffledArray.includes(currentArray[i])) {
+        shuffledArray.push(
+          currentArray[Math.floor(Math.random() * currentArray.length)]
+        );
+      }
+    }
+    shuffledArray = this.choices;
+    return this.choices;
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,47 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions; // array
+    this.timeLimit = timeLimit; // number
+    this.timeRemaining = timeRemaining; // number
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 4. shuffleQuestions()
+  // shuffleQuestions() {
+  //   let currentQuestion = this.choices; //change this!!!!!!!!!!!!
+  //   let shuffledArray = [currentArray[0]];
 
-    // 5. checkAnswer(answer)
+  //   for (let i = 0; i < currentArray.length; i++) {
+  //     if (!shuffledArray.includes(currentArray[i])) {
+  //       shuffledArray.push(
+  //         currentArray[Math.floor(Math.random() * currentArray.length)]
+  //       );
+  //     }
+  //   }
+  //   shuffledArray = this.choices;
+  //   return this.choices;
+  // }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    if (answer === true) {
+      this.correctAnswers++;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if ((this.currentQuestionIndex = this.questions.length)) {
+      return true;
+    }
+    return undefined;
+  }
 }
