9385e20b0656072d166d85b595532563388a2abc
Maksym <citomax7196@gmail.com>
2024-01-15 15:54:40 +0100

    question and quiz tasks done
    Co-authored-by: VladysslavTkach <VladysslavTkach@users.noreply.github.com>

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text
+        this.choices = choices
+        this.answer = answer
+        this.difficulty = difficulty
+    }
 
-    // 2. shuffleChoices()
+    shuffleChoices() {
+        const newChoices = [...this.choices];
+        let currentIndex = newChoices.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = newChoices[currentIndex];
+            newChoices[currentIndex] = newChoices[randomIndex];
+            newChoices[randomIndex] = temporaryValue;
+        }
+        this.choices = newChoices;
+        return this.choices;
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,42 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
+  shuffleQuestions() {
+    const newQuestionsArray = [...this.questions];
+    let currentIndex = newQuestionsArray.length,
+      temporaryValue,
+      randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = newQuestionsArray[currentIndex];
+      newQuestionsArray[currentIndex] = newQuestionsArray[randomIndex];
+      newQuestionsArray[randomIndex] = temporaryValue;
+    }
+    this.questions = newQuestionsArray;
+    return this.questions;
+  }
+  checkAnswer(answer) {
+    if (typeof answer === 'string') {
+      this.correctAnswers++;
+    }
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
