5210728d00e3c4f7629781d2c3b8c19ec36b60e0
Deniz Cilingir <deniz.cilingir@gmx.de>
2024-01-15 18:02:35 +0100

    under construction

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,51 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
+        
+    moveToNextQuestion () {
+        this.currentQuestionIndex += 1;
+        this.getQuestion();
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        const newQuestions = this.questions.slice();
+        for (let i = this.questions.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = this.questions[i];
+          newQuestions[i] = this.questions[j];
+          newQuestions[j] = temp;
+        }
+        this.questions = newQuestions;
+        return this.questions;
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        let currentQuestion = this.questions[this.currentQuestionIndex];
+        if (currentQuestion.answer === answer) {
+            this.correctAnswers++;
+        }
+        
+    }
 
-    // 6. hasEnded()
+    hasEnded() {
+        let hasEnded = false;
+        if (this.currentQuestionIndex === this.questions.length) { 
+            hasEnded = true;
+        }
+        return hasEnded;
+    }
 }
+
+/* hasEnded() {
+    return this.currentQuestionIndex === this.questions.length;
+}
+} */
