bd630bc9f106896a5e2eac6994f1d55f6bcb8398
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-01-22 17:14:21 +0100

    done the today tasks

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+      this.questions = questions;
+      this.timeLimit = timeLimit;
+      this.timeRemaining = timeRemaining;
+      this.correctAnswers = 0;
+      this.currentQuestionIndex = 0;
+    }
+    getQuestion() {
+      return this.questions[this.currentQuestionIndex];
+    }
+    moveToNextQuestion() {
+      this.currentQuestionIndex++;
+    }
+    shuffleQuestions() {
+        for (let i = 0; i < this.questions.length; i++) {
+      let randomQuestion = Math.floor(Math.random() * this.questions.length);
+      [this.questions[i], this.questions[this.questions]] = [
+        this.questions[randomQuestion],
+        this.questions[i],
+      ];
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    }
+    checkAnswer(answer) {
+        if (this.answer === this.choice) {
+            this.correctAnswers++
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
-}
+        }
+    }
+    hasEnded() {
+      if (this.currentQuestionIndex < this.questions.length) {
+        return false;
+      } else if (this.currentQuestionIndex == this.questions.length) {
+        return true;
+      }
+    }
+  }
