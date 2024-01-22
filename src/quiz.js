aea078b3746ac5a98785534df25c7b56c222d36e
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-01-22 16:57:20 +0100

    first day done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,39 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+   constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+   }
+    getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+    this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for(let i = this.questions.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+            }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+      this.correctAnswers++;
 
-    // 6. hasEnded()
+    }
+
+    hasEnded() {
+    if(this.currentQuestionIndex < this.questions.length){
+    return false;
+    }else{
+    return true;
+    }
+
+    }
 }
