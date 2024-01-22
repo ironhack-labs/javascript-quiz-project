eb0fbad94b89dd26163f43a676606bf55600f49e
Tomás Bastos <soutomasbastos@gmail.com>
2024-01-22 18:05:18 +0000

    first part js quiz

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,47 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
+  // 4. shuffleQuestions()
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = 0; i < this.questions.length; i++) {
+      let j = Math.floor(Math.random() * this.questions.length - 1);
+      let tempVar = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = tempVar;
+    }
+    return this.questions;
+  }
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    this.correctAnswers += 1;
+  }
+  // 6. hasEnded()
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
