5fd9b2026071f4f44cad32248497b63ae54ab3f4
Teresa <tereac1102@gmail.com>
2024-01-22 17:51:58 +0100

    work done!

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,69 @@
+
+// 1. constructor (questions, timeLimit, timeRemaining)
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questionsValue, timeLimitValue, timeRemainingValue) {
+    this.questions = questionsValue
+     this.timeLimit = timeLimitValue
+     this.timeRemaining = timeRemainingValue
+    this.correctAnswers = 0
+    this.currentQuestionIndex = 0
+
+    }
 
     // 2. getQuestion()
-    
+    getQuestion() {
+        const currentQuestion = this.questions[this.currentQuestionIndex]
+        return currentQuestion
+
+}
     // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        return this.currentQuestionIndex++
+    }
 
     // 4. shuffleQuestions()
+
+    shuffleQuestions() {
+        this.questions.sort(() => Math.random() - 0.5);
+    }
+
+
+
+
+    checkAnswer(answer) {
+        return this.correctAnswers++
+    }
+
+    hasEnded() {
+
+        if (this.currentQuestionIndex === this.questions.length) {
+            return true  
+        } 
+        else {
+            return false
+        }
+    }
+}
+
+    
+
+
+
+
+
+
+
+
+// YOUR CODE HERE:
+    //
+   
+
+
+    
+  
+
+
 
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-}
