5864aa046aa9b6baa5896ea845b05bbf790e97f8
Barbara Lan <barbie.lancuba@gmail.com>
2024-01-22 17:38:23 +0100

    day 1 work complete

--- src/question.js
+++ src/question.js
@@ -1,7 +1,25 @@
 class Question {
     // YOUR CODE HERE:
-    //
     // 1. constructor (text, choices, answer, difficulty)
-
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
     // 2. shuffleChoices()
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i));
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+        }
+        return this.choices;
+    }
 }
+
+
+const q1 = new Question ('hola', ['a','b','c','d'], 'que tal', 1)
+ console.log(q1.shuffleChoices())
+
+
+//console.log(q1.shuffleChoices(q1.choices)) 
