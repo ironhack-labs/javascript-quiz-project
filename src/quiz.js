45890d0c696009fdeee274faf85ba035e5f44ac6
chyVacheck <dmytro.shah.work@gmail.com>
2024-01-22 16:23:02 +0100

    done quiz

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,47 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      console.log("j: ", j);
 
-    // 6. hasEnded()
+      const _element = this.questions[j];
+
+      this.questions[j] = this.questions[i];
+      this.questions[i] = _element;
+    }
+
+    console.log(this.questions);
+  }
+
+  checkAnswer(answer) {
+    if (this.getQuestion().answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  hasEnded() {
+    return this.currentQuestionIndex === this.questions.length;
+  }
 }
+
+const quiz = new Quiz(["apple", "orange", "cucumber", "123", "456"], "", "");
+
+quiz.shuffleQuestions();
