3434844ab7a2b56ca6f932a1df3276a85b55b0fa
Filipe Teixeira <filipe942001teixeira@gmail.com>
2024-01-22 16:35:58 +0000

    Day1 of the mini-project done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,45 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
 
-    // 4. shuffleQuestions()
+    moveToNextQuestion(){
+        this.currentQuestionIndex += 1;
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions(){
+        for(let i=0; i<this.questions.length; i++){
+            let j = Math.floor(Math.random()*this.questions.length-1);
+            let tempVar = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = tempVar;
+        }
+        return this.questions
+    }
 
-    // 6. hasEnded()
+    /* Doesn't check the answer, only increments the amount of 
+    correct answers */
+    checkAnswer(answer){
+        return this.correctAnswers ++
+    }
+
+    hasEnded(){
+        if(this.currentQuestionIndex < this.questions.length){
+            return false;
+        }
+        else if(this.currentQuestionIndex === this.questions.length){
+            return true
+        }
+    }
 }
+
+
+// 6. hasEnded()
