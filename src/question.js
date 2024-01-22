4f03d4b39bcadf4408fd814b86ad8715c0492428
Veronica Vezzoli <veronicavezzoli94@gmail.com>
2024-01-22 16:25:28 +0100

    done

--- src/question.js
+++ src/question.js
@@ -4,4 +4,13 @@ class Question {
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
+    constructor(textValue, choicesValue, answerValue, difficultyValue){
+        this.text = textValue  
+        this.choices = choicesValue
+        this.answer = answerValue
+        this.difficulty = difficultyValue
+    }
+    shuffleChoices(){
+      this.choices.sort(() => Math.random() - 0.5)
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,35 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor(questionsValue, timeLimitValue, timeRemainingValue){
+        this.questions = questionsValue
+        this.timeLimit = timeLimitValue
+        this.timeRemaining = timeRemainingValue
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
     // 2. getQuestion()
-    
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
     // 3. moveToNextQuestion()
-
+    moveToNextQuestion(){
+        return this.questions[this.currentQuestionIndex++]
+    }
     // 4. shuffleQuestions()
-
+    shuffleQuestions(){
+        this.questions.sort(() => Math.random() -0.5)
+    }
     // 5. checkAnswer(answer)
-
+    checkAnswer(answer){
+        if(answer){
+        this.correctAnswers++
+        }
+    }
     // 6. hasEnded()
+    hasEnded(){
+        if(this.currentQuestionIndex === this.questions.length){
+        return true
+        } else {return false}
+    }
 }
