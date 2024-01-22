352f13ec2e8b31be601daf85340bf390c9a0b669
Stefan.Kummerlöw <gummiz@gmx.net>
2024-01-22 17:23:31 +0100

    done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,27 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty; //Note: The difficulty will be a number between 1 and 3, with 1 being the easiest and 3 being the hardest.
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    //Fisher-Yates Shuffle Algorithm
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = this.choices[i];
+      this.choices[i] = this.choices[j];
+      this.choices[j] = temp;
+    }
+
+    console.log(this.choices);
+  }
 }
+
+// const question1 = new Question(
+//   "some text",
+//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+//   "more text"
+// );
+// question1.shuffleChoices();
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,53 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
     // 2. getQuestion()
+    getQuestion(){
+        console.log(this.questions[this.currentQuestionIndex])
+        return this.questions[this.currentQuestionIndex]
+    }
     
     // 3. moveToNextQuestion()
-
+    moveToNextQuestion() {
+        this.currentQuestionIndex++
+    }
     // 4. shuffleQuestions()
-
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+          }
+        return
+    }
     // 5. checkAnswer(answer)
-
+    checkAnswer(answer) {
+        if (answer) {
+            this.correctAnswers++
+        }
+    }
     // 6. hasEnded()
+    hasEnded() {
+        console.log(this.questions.length)
+        if (this.currentQuestionIndex === this.questions.length){
+            return true
+        } else {
+            return false
+        }
+    }
 }
+
+
+const myQuiz1 = new Quiz(["Question 1:...", "Question 2:...", "Question 3:..." ,"Question 4:..." ], 60, 60)
+
+myQuiz1.getQuestion()
+myQuiz1.checkAnswer("my Answer")
+myQuiz1.hasEnded()
