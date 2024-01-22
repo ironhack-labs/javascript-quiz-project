a2f45b6ccd48605d271fb01030e95bac07a069c8
Simona Wurst <simona_wu@yahoo.de>
2024-01-22 16:50:47 +0100

    Day One complete

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,47 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions,timeLimit,timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+    for (let i = this.questions.length - 1; i > 0; i--) { 
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]]; 
+      } 
+      // return this.questions; 
+   }; 
 
-    // 5. checkAnswer(answer)
 
-    // 6. hasEnded()
+    checkAnswer(answer){
+     if (typeof answer === "string"){
+        this.correctAnswers++; 
+        //return answer === "true" ? correctAnswers ++ : "not right!" 
+        //if(!arr.length)
+    }
+
+}
+
+hasEnded(){
+   return this.currentQuestionIndex === this.questions.length ? true : false;
+
+  /* if(this.currentQuestionIndex === this.questions.length){
+    return true;
+   }else if(this.currentQuestionIndex < this.questions.length ){
+    return false;
+   }*/
+}
 }
