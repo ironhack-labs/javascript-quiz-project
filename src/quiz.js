e63000daf9f92a78424481d35819d26e8d0ea78c
Tushar16-1998 <tusharsunder2@gmail.com>
2024-01-22 18:00:34 +0000

    done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions ;
+        this.timeLimit = timeLimit ;
+        this.timeRemaining = timeRemaining ;
+        this.correctAnswers = 0 ;
+        this.currentQuestionIndex = 0 ;
+    }
     
-    // 3. moveToNextQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
+    
+    moveToNextQuestion(){
+        this.currentQuestionIndex++
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        for(let i = 0; i < this.questions.length; i++){
+            let j = Math.floor(Math.random()*this.questions.length-1);
+            let tempVar = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = tempVar;
+        }
+        return this.questions
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        const currentQuestion = this.getQuestion();
+        if(answer === currentQuestion.answer){
+            this.correctAnswers++
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded(){
+        return this.currentQuestionIndex === this.questions.length
+    }
 }
