75f6097e36916d7f4bf040c6b1be07ddb30fac0c
Alejandro Magro <alejandromagro1@gmail.com>
2024-01-22 17:28:39 +0100

    Done day1 javascript-quiz-project

--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,48 @@
+// YOUR CODE HERE:
+    
 class Quiz {
-    // YOUR CODE HERE:
-    //
+    constructor(questionsValue, timeLimitValue, timeRemainingValue) {
+        
+        this.questions = questionsValue
+        this.timeLimit = timeLimitValue
+        this.timeRemaining = timeRemainingValue
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
+        getQuestion() {
+           return this.questions[this.currentQuestionIndex]
+
+        }
+
+        moveToNextQuestion() {
+            this.currentQuestionIndex++
+
+        }
+
+        shuffleQuestions() {
+            this.questions.sort(() => Math.random() - .5)
+        }
+
+        checkAnswer(answer) {
+            this.correctAnswers++
+
+        }
+
+        hasEnded() {
+            if(this.currentQuestionIndex === this.questions.length){
+                return true
+
+        }
+        else {
+            return false
+        }
+
+
+    }
+
+}
+
+
     // 1. constructor (questions, timeLimit, timeRemaining)
 
     // 2. getQuestion()
@@ -12,4 +54,8 @@ class Quiz {
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-}
+
+
+     
+
+
