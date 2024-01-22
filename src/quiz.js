a63926600d260f1d5ccf551b1d5dc172b3e98a8b
Daniel_Costa <dan.costaa.12@gmail.com>
2024-01-22 17:24:13 +0000

    daniel commit day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,31 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions,timeLimit,timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
+    moveToNextQuestion(){
+        return this.currentQuestionIndex += 1
+    }
+    shuffleQuestions(){
+        return this.questions.sort(()=>Math.random() - 0.5)
+    }
 
-    // 2. getQuestion()
+    checkAnswer(answer){
+        
+        return this.correctAnswers += 1
     
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    }
+    hasEnded(){
+        if (this.currentQuestionIndex < this.questions.length){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
