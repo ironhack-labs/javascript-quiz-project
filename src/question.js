5738d6bd431fd10f8990698b2605058fa057614c
Alastair Longmuir <alastairlinwood@gmail.com>
2024-01-22 16:14:31 +0100

    day one complete

--- src/question.js
+++ src/question.js
@@ -2,6 +2,23 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
     // 2. shuffleChoices()
+    shuffleChoices() {
+        this.choices.sort(() => Math.random() - 0.5);
+
+        // let tempChoices = this.choices;
+        // let mixedChoices = [];
+        // this.choices.forEach(function() {
+        //     const randomNumber = Math.floor(Math.random() * tempChoices.length);
+        //     mixedChoices.push(tempChoices[randomNumber]);
+        //     tempChoices.splice(randomNumber, 1)
+        // });
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,34 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        this.questions.sort(() => Math.random() - 0.5);
 
-    // 5. checkAnswer(answer)
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer) {
+        if (this.questions[this.currentQuestionIndex].answer === answer) {
+            this.correctAnswers++;
+        }
+    }
+
+    hasEnded() {
+        return this.currentQuestionIndex < this.questions.length ? false : true;
+    }
 }
