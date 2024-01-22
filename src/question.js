ef923cc0681b1887f58d5f6de21cc048c7a6e2d6
pouria pou <pouriap024@yahoo.com>
2024-01-22 16:43:24 +0100

    day 1 paired project

--- src/question.js
+++ src/question.js
@@ -1,7 +1,21 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
 
-    // 2. shuffleChoices()
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  // 2. shuffleChoices()
+  shuffleChoices() {
+    this.newArr = [];
+    // console.log("choices before change", this.choices);
+    // for (let i = this.choices.length - 1; i >= 0; i--) {
+    //   this.newArr.push(this.choices[i]);
+    // }
+    // console.log("choices after change", this.newArr);
+  }
 }
