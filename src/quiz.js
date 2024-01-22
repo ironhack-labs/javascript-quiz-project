ba906d1606d4de05912a7f8cc684df9198b6364a
Jeronimo Olea <jerooleag2004@gmail.com>
2024-01-22 15:46:40 +0100

    DONEEEEE!

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,32 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
+    moveToNextQuestion(){
+        this.currentQuestionIndex += 1
+    }
+    shuffleQuestions(){
+        this.questions.sort(() => Math.random() - 0.5)
+      return this.questions
+    }
+    checkAnswer(answer){
+        if (answer){
+            this.correctAnswers +=1
+        }
+    }
+    hasEnded(){
+        if (this.currentQuestionIndex < this.questions.length){
+            return false
+        }
+        else if(this.currentQuestionIndex === this.questions.length){
+            return true
+        }
+    }
 }
