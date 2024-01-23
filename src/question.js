6aee9bc4b563157a5dcbba858467f4e5ae5326a6
Diana Ramos <diana.sd.ramos@gmail.com>
2024-01-23 22:28:43 +0000

    Day 2

--- src/question.js
+++ src/question.js
@@ -1,19 +1,19 @@
 class Question {
     constructor (text , choices , answer , difficulty) {
-        this.text = text ;
-        this.choices = choices ;
-        this.answer = answer ;
-        this.difficulty = difficulty ; 
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty; 
     }
 
     shuffleChoices() {
-        for(let i=0; i<this.choices.length; i++){
-            let j = Math.floor(Math.random()*this.choices.length-1);
+        for (let i = 0; i < this.choices.length; i++) {
+            let j = Math.floor(Math.random() * this.choices.length - 1);
             let tempVar = this.choices[i];
             this.choices[i] = this.choices[j];
             this.choices[j] = tempVar;
         }
-        return this.choices
+        return this.choices;
  
     }     
 
--- src/quiz.js
+++ src/quiz.js
@@ -1,38 +1,38 @@
 class Quiz {
     constructor (questions, timeLimit, timeRemaining) {
-        this.questions = questions ;
-        this.timeLimit = timeLimit ;
-        this.timeRemaining = timeRemaining ;
-        this.correctAnswers = 0 ;
-        this.currentQuestionIndex = 0 ;
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
     }
     
-    getQuestion(){
-        return this.questions[this.currentQuestionIndex]
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
     }
     
-    moveToNextQuestion(){
+    moveToNextQuestion() {
         this.currentQuestionIndex++;
     }
 
-    shuffleQuestions(){
-        for(let i = 0; i < this.questions.length; i++){
-            let j = Math.floor(Math.random()*this.questions.length-1);
+    shuffleQuestions() {
+        for (let i = 0; i < this.questions.length; i++){
+            let j = Math.floor(Math.random() * this.questions.length - 1);
             let tempVar = this.questions[i];
             this.questions[i] = this.questions[j];
             this.questions[j] = tempVar;
         }
-        return this.questions
+        return this.questions;
     }
 
-    checkAnswer(answer){
+    checkAnswer(answer) {
         const currentQuestion = this.getQuestion();
-        if(answer === currentQuestion.answer){
-            this.correctAnswers++
+        if (answer === currentQuestion.answer) {
+            this.correctAnswers++;
         }
     }
 
-    hasEnded(){
-        return this.currentQuestionIndex === this.questions.length
+    hasEnded() {
+        return this.currentQuestionIndex === this.questions.length;
     }
 }
