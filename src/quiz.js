a2ba7798ee90fe7c6a25e47e90156e6e48cdc35a
TiagoSilvina <tiagoafsilvina@gmail.com>
2024-01-23 17:07:26 +0000

    final change

--- src/quiz.js
+++ src/quiz.js
@@ -28,18 +28,9 @@ class Quiz {
             return true;
         }
     }
-<<<<<<< HEAD
-    filterQuestionsByDifficulty(difficulty){
-    return this.questions.filter((question)=>{
-        return question.difficulty === difficulty
-    })
-    }
-}
-=======
     filterQuestionsByDifficulty(difficulty) {
         return this.questions.filter((question) => {
             return question.difficulty === difficulty;
         });
     }
 }
->>>>>>> 5c85410e98ec84ee90b04b1ebaf3031fe2ae9d6c
