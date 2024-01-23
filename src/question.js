09173937ad73ec707b60d5947c77d4cf188289a1
Daniel_Costa <dan.costaa.12@gmail.com>
2024-01-23 17:10:47 +0000

    merge conflict

--- src/question.js
+++ src/question.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-class Question{
-    constructor(text, choices,answer, difficulty){
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-        this.difficulty = difficulty;
-    }
-    shuffleChoices(){
-         return this.choices.sort(() => Math.random() - 0.5)
-}
-}
-=======
 class Question {
     constructor(text,choices,answer,difficulty){
         this.text=text;
@@ -22,4 +9,3 @@ class Question {
         return this.choices.sort(()=>Math.random() - 0.5)
 }
 }
->>>>>>> 5c85410e98ec84ee90b04b1ebaf3031fe2ae9d6c
