bb409c33ff0cdf3d6f6051a2c7eefef12351bb03
Leonardo Oliveira <leooliveira1025@gmail.com>
2024-01-23 09:23:43 +0000

    day 1 already solved, day 2 started first exercice

--- src/quiz.js
+++ src/quiz.js
@@ -1,37 +1,63 @@
+// YOUR CODE HERE:
+//
+// 1. constructor (questions, timeLimit, timeRemaining)
+
 class Quiz {
-constructor (questions, timeLimit, timeRemaining){
-    this.questions = questions;
-    this.timeLimit = timeLimit;
-    this.timeRemaining = timeRemaining;
-    this.correctAnswers = 0;
-    this.currentQuestionIndex = 0;
-}
-getQuestion(){
-    return this.questions[this.currentQuestionIndex];
-}
-    
-moveToNextQuestion(){
-    this.currentQuestionIndex++;
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    // 2. getQuestion()
+
+    getQuestion() {
+
+        return this.questions[this.currentQuestionIndex]
+    }
+
+    moveToNextQuestion() {
+
+        this.currentQuestionIndex++;
+
+    }
+
+    shuffleQuestions() {
+
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+
+    }
+
+    checkAnswer(answer) {
+
+        this.correctAnswers++;
+
+    }
+
+    hasEnded() {
+
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        }
+
+        if (this.currentQuestionIndex = this.questions.length) {
+            return true;
+        }
+
+    }
+
 }
 
-shuffleQuestions(){
-    for (let i = this.questions.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
-      }
-}
 
-checkAnswer(answer){
-    this.correctAnswers++;
-}
+// 3. moveToNextQuestion()
 
-hasEnded(){
-    if (this.currentQuestionIndex<this.questions.length) {
-    return false
-     } if (this.currentQuestionIndex===this.questions.length)
-     return true
-    } 
+// 4. shuffleQuestions()
 
-    filterQuestionsByDifficulty(){}
-}
+// 5. checkAnswer(answer)
 
+// 6. hasEnded()
