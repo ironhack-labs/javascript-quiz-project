733d20270c411767832cf91208478e9d85926985
“Filipe.Santiago” <thekyd42@gmail.com>
2024-01-27 18:29:35 +0000

    Solved Quiz Filipe-Vieira & Piyush-Bisht

--- src/quiz.js
+++ src/quiz.js
@@ -1,40 +1,65 @@
 class Quiz {
-  
-        constructor (questions, timeLimit, timeRemaining){
-            this.questions = questions;
-            this.timeLimit = timeLimit;
-            this.timeRemaining = timeRemaining;
-            this.correctAnswers = 0;
-            this.currentQuestionIndex = 0;
-          
-        }
 
-        getQuestion(){
-            return this.questions[this.currentQuestionIndex];
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+
+    }
+
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
+
+    moveToNextQuestion() {
+        return this.currentQuestionIndex += 1;
+    }
+
+    shuffleQuestions() {
+        function shuffle(array) {
+            array.sort(() => Math.random() - 0.5);
         }
+        return shuffle(this.questions);
+    }
+
+    checkAnswer(answer) { // should increase 'correctAnswers' by 1 when a correct answer is passed as an argument
+        const question = this.getQuestion()
+        const isCorrectAnswer = question.answer === answer
+        if (isCorrectAnswer) {
+            this.correctAnswers++
+        }
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    filterQuestionsByDifficulty(difficulty) {
+
+        if (difficulty >=1 && difficulty <=3 ){
+        const dif1 = this.questions.filter((question) => {
             
-        moveToNextQuestion(){
-            return this.currentQuestionIndex += 1; 
-        }
-        
-        shuffleQuestions(){
-            function shuffle(array) {
-                array.sort(() => Math.random() - 0.5);
-              }
-              return shuffle(this.questions);
-        }
-
-        checkAnswer(answer){ // should increase 'correctAnswers' by 1 when a correct answer is passed as an argument
-        return this.correctAnswers += 1;
-
-        }
-
-        hasEnded(){
-            if (this.currentQuestionIndex < this.questions.length){
-                return false;
-            }
-            else {
-                return true;
-            }
-        }
+            return question.difficulty === difficulty;
+        });
+     return this.questions = dif1;
+    }
 }
+
+    averageDifficulty() {
+        
+        const totalDifficulty = this.questions.reduce((accumulator, question) => {
+            return accumulator + question.difficulty;
+        },0);
+    
+        return totalDifficulty / this.questions.length;
+    }
+}
+
+
