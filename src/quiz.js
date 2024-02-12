f725e3728d21e46d3ed81d231ad76ffa15e7abcb
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-12 19:32:27 -0400

    questions js fully donde, working on quiz js

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,80 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    } // end of constructor
     
-    // 3. moveToNextQuestion()
+    getQuestion() {
+// return {this.currentQuestionIndex}
+    } // end of gotQuestion method
+    
+    moveToNextQuestion() {
 
-    // 4. shuffleQuestions()
+    } // end of moveToNextQuestion method
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions() {
 
-    // 6. hasEnded()
-}
+    } // end of shuffleQuestions
+
+    checkAnswer(answer) {
+
+    } // end of checkAnswer
+    
+    hasEnded() {
+
+    } //end of hasEnded 
+} // end of Quiz class
+
+
+// getQuestion() method
+
+// Returns the question from the questions array at the position of currentQuestionIndex.
+
+// should be defined.
+// should be a function.
+// should receive no arguments.
+// should return the item from the questions array at the position of currentQuestionIndex.
+
+// moveToNextQuestion() method
+
+// When called, increments the currentQuestionIndex by 1.
+
+// should be defined.
+// should be a function.
+// should receive no arguments.
+// should increment the currentQuestionIndex by 1.
+
+// shuffleQuestions() method
+
+// Shuffles the elements stored in the questions array of the Quiz.
+
+// should be defined.
+// should be a function.
+// should receive no arguments.
+// should shuffle the items in the questions array.
+
+// checkAnswer(answer) method
+
+// Checks if the passed answer is correct for the current question and increments correctAnswers by 1 if the answer is correct.
+
+// should be defined.
+// should be a function.
+// should receive 1 argument (answer - string).
+// should increase correctAnswers by 1 when called with a correct answer for the current question
+
+// hasEnded() method
+
+// Returns true if the quiz has ended (the last question has been answered), and false otherwise.
+
+// should be defined.
+
+// should be a function.
+
+// should receive no arguments.
+
+// should return false when currentQuestionIndex is less than the questions array length
+
+// should return true when currentQuestionIndex is equal to the questions array length
