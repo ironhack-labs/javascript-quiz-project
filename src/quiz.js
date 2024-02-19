f847e0873ee2c75acb15c89ca5542ee341b82e54
PaulinaWW <159482754+PaulinaWW@users.noreply.github.com>
2024-02-19 17:27:39 +0100

    Co-authored-by: mmisurell <mmisurell@users.noreply.github.com>

--- src/quiz.js
+++ src/quiz.js
@@ -2,12 +2,30 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+
+    }
 
     // 2. getQuestion()
-    
+    getQuestion() {
+
+        return this.questions(this.currentQuestionIndex);
+    }
     // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+
+        this.currentQuestionIndex++;
+    }
 
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+}
+
 
     // 5. checkAnswer(answer)
 
