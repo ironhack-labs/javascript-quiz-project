1a8e1773ea9bfa5595428bd7ef3d563696c88f9e
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-19 16:23:15 +0100

    randomizing with the Fisher-Yates algorithm yo

--- src/question.js
+++ src/question.js
@@ -5,5 +5,12 @@ class Question {
     this.answer = answer;
     this.difficulty = difficulty;
   }
-  shuffleChoices() {}
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = this.choices[i];
+      this.choices[i] = this.choices[j];
+      this.choices[j] = temp;
+    }
+  }
 }
