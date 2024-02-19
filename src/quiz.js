1951ddd4a5b5b8bfd5cf457d454d91b6867682c7
Adrian Doonan <adriandoonan@gmail.com>
2024-02-19 16:21:59 +0100

    quiz class

--- src/quiz.js
+++ src/quiz.js
@@ -10,22 +10,24 @@ class Quiz {
     }
 
     getQuestion() {
-
+        return this.questions[this.currentQuestionIndex]
     }
     
     moveToNextQuestion() {
-
+        this.currentQuestionIndex++
     }
 
     shuffleQuestions() {
-
+        this.questions.sort((a,b) => Math.random() >= Math.random() ? -1 : 1 )
     }
 
     checkAnswer(answer) {
-
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
+            this.correctAnswers++
+        }
     }
 
     hasEnded() {
-
+        return this.currentQuestionIndex === this.questions.length
     }
 }
