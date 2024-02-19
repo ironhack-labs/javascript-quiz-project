d170306515b3d19e416f1280bcb7031297be21ae
Adrian Doonan <adriandoonan@gmail.com>
2024-02-19 16:13:12 +0100

    quiz class

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,31 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion() {
+
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
 
-    // 4. shuffleQuestions()
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions() {
 
-    // 6. hasEnded()
+    }
+
+    checkAnswer(answer) {
+
+    }
+
+    hasEnded() {
+
+    }
 }
