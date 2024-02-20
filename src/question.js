469a21cf5f95122fd1d62d48a4efac1e3f8e7cc6
Luiz-elTlaloc <el.tlaloc1316@gmail.com>
2024-02-20 08:22:12 -0600

    mini-proyect-solved

--- src/question.js
+++ src/question.js
@@ -2,6 +2,20 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
-
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
     // 2. shuffleChoices()
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) {
+            // Generate a random index between 0 and i (inclusive)
+            const j = Math.floor(Math.random() * (i + 1));
+            // Swap elements at indices i and j
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+          }
+          return this.choices;
+    }
 }
