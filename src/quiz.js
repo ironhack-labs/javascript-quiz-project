01c8c10b7e62c1eed3417a496b545c97e5abaebc
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-20 09:41:04 +0100

    working on checkAnswer

--- src/quiz.js
+++ src/quiz.js
@@ -12,11 +12,22 @@ class Quiz {
     return this.questions[this.currentQuestionIndex];
   }
 
-  moveToNextQuestion() {}
+  moveToNextQuestion() {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex += 1;
+    }
+  }
 
-  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = temp;
+    }
+  }
 
-  // 5. checkAnswer(answer)
+  checkAnswer(answer) {}
 
   // 6. hasEnded()
 }
