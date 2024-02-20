60522f0bb6ea87765f2cf0556cc35e5aa02eaaa7
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-20 17:10:28 +0100

    moveToNextQuestion is preciser now

--- src/quiz.js
+++ src/quiz.js
@@ -8,42 +8,37 @@ class Quiz {
     this.currentQuestionIndex = 0;
   }
 
-  getQuestion () {
-   return this.questions[this.currentQuestionIndex]
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
   }
-  moveToNextQuestion(){
-    this.currentQuestionIndex += 1;
-
+  moveToNextQuestion() {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex += 1;
+    }
   }
 
-  shuffleQuestions(){
+  shuffleQuestions() {
     for (let i = this.questions.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = this.questions[i];
       this.questions[i] = this.questions[j];
       this.questions[j] = temp;
     }
-
   }
 
-  checkAnswer(answer){
-    this.questions.forEach(question => {
-if (answer === question.answer){
-
-  this.correctAnswers += 1;
-}
-
-    })
-
+  checkAnswer(answer) {
+    this.questions.forEach((question) => {
+      if (answer === question.answer) {
+        this.correctAnswers += 1;
+      }
+    });
   }
 
- hasEnded(){
-if (this.currentQuestionIndex < this.questions.length){
-
-  return false;
-} else {
-  return true;
-}
-
- }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
