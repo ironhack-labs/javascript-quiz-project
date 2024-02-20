ef4714e84900dc3a26173267639f620e7befc461
Memoryman2022 <rlacannon@gmail.com>
2024-02-20 23:16:13 +0100

    day2

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,81 @@
+// YOUR CODE HERE:
+// 1. constructor (questions, timeLimit, timeRemaining)
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
 
-    // 4. shuffleQuestions()
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    let currentIndex = this.questions.length;
+    let randomIndex = 0;
 
-    // 5. checkAnswer(answer)
+    while (currentIndex > 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
 
-    // 6. hasEnded()
+      [this.questions[currentIndex], this.questions[randomIndex]] = [
+        this.questions[randomIndex],
+        this.questions[currentIndex],
+      ];
+    }
+
+    return this.choices;
+  }
+
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer === this.questions[this.currentQuestionIndex].answer) {
+      return this.correctAnswers++;
+    }
+  }
+  // 6. hasEnded()
+
+  hasEnded() {
+    console.log(this.currentQuestionIndex);
+    console.log(this.questions.length);
+
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    }
+    return true;
+  }
+
+  filterQuestionsByDifficulty(difficulty) {
+    difficulty = Number(difficulty);
+
+    if (difficulty > 3 || difficulty < 1) {
+      return this.questions;
+    }
+    this.questions = this.questions.filter(
+      (currentQuestion) => currentQuestion.difficulty === difficulty
+    );
+
+    return this.questions;
+  }
+
+  // AverageDifficulty
+
+  averageDifficulty() {
+    const totalDifficulty = this.questions.reduce(
+      (acc, question) => acc + question.difficulty,
+      0
+    );
+
+    const averageDifficulty = totalDifficulty / this.questions.length;
+    return averageDifficulty;
+  }
 }
