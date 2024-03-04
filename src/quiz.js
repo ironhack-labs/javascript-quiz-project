264da229648a2f27aeb98bcd680774331a754bb4
Alexis <alexismoreauavila@gmail.com>
2024-03-04 17:54:58 +0100

    finished day one

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,45 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  shuffleQuestions() {
+    let i = 0;
+    let temp = "";
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    while (i < this.questions.length) {
+      let random = Math.floor(Math.random() * this.questions.length);
+      let randomNum = this.questions[random];
+      temp = this.questions[i];
+      this.questions[i] = randomNum;
+      this.questions[random] = temp;
+      i++;
+    }
+    return this.questions;
+  }
 
-    // 4. shuffleQuestions()
+  checkAnswer(answer) {
+    if (this.getQuestion().answer === answer) {
+      this.correctAnswers++;
+    }
+    console.log(this.getQuestion());
+    console.log(answer);
+  }
 
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
