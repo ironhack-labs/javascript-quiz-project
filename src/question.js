8503ec21cb55cbebcdf2842a035124cc63583c08
smnraphael <simonraphael@hotmail.fr>
2024-03-04 17:20:29 +0100

    questions done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,60 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    let i = 0;
+    let temp = "";
+
+    while (i < this.choices.length) {
+      let random = Math.floor(Math.random() * this.choices.length);
+      let randomNum = this.choices[random];
+      temp = this.choices[i];
+      this.choices[i] = randomNum;
+      this.choices[random] = temp;
+      i++;
+    }
+    return this.choices;
+  }
+
+  //     for (let i = 0; i < this.choices.length; i++) {
+  //       let random = Math.floor(Math.random() * this.choices.length);
+  //       let randomNum = this.choices[random];
+  //       temp = this.choices[i];
+  //       this.choices[i] = randomNum;
+  //       this.choices[random] = temp;
+  //     }
+  //     return this.choices;
+  //   }
+
+  //     let i = 0;
+  //     let newArr = [];
+
+  // //     while (i < this.choices.length) {
+  //       let random = Math.floor(Math.random() * this.choices.length);
+  //       let randomNum = this.choices[random];
+  //       if (newArr.includes(this.choices[random])) {
+  //         continue;
+  //       } else {
+  //         newArr.push(randomNum);
+  //       }
+  //       i++;
+  //     }
+  //     this.choices = structuredClone(newArr);
+  //     return this.choices;
+  //   }
+
+  test() {
+    return this.choices;
+  }
 }
+
+const a = new Question("hello", ["a", "b", "c", "d", "e", "f"], "a", 100);
+const b = new Question("hellb", ["d", "e", "f"], "d", 53);
+const c = new Question("hellc", ["g", "h", "i"], "h", 92);
+
+console.log(a.shuffleChoices());
