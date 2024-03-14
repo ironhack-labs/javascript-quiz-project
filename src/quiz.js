61e8ee719e40c3ae250a875f97d9d3665f5e12d5
alex.laycy <alexlaycy@gmail.com>
2024-03-14 20:31:54 +0000

    Day 1 - Iterations 1 to 5

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,51 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
+  // 2. getQuestion()
+
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  // 3. moveToNextQuestion()
+
+  moveToNextQuestion() {
+    return this.questions[this.currentQuestionIndex++];
+  }
+
+  // 4. shuffleQuestions()
+
+  shuffleQuestions() {
+    for (let i = 0; i < this.questions.length; i++) {
+      let j = Math.floor(Math.random() * this.questions.length);
+
+      let temp = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = temp;
+    }
+
+    return this.questions;
+  }
+
+  // 5. checkAnswer(answer)
+
+  checkAnswer(answer) {
+
+    let  currentQuestion = this.questions[this.currentQuestionIndex]
+    if (answer ===  currentQuestion.answer) {
+      this.correctAnswers++;
+    }
+    return this.correctAnswers
+  }
     
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  // 6. hasEnded()
 }
