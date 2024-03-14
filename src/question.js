cab3d8977de902b3ff6249b7393f18f155b3274b
Michael Nguyen <michael.nguyen@live.nl>
2024-03-14 21:09:27 +0100

    shufflechoices method done

--- src/question.js
+++ src/question.js
@@ -8,5 +8,11 @@ class Question {
 
      shuffleChoices(){
         
+            for (let i = this.choices.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              const temp = this.choices[i];
+              this.choices[i] = this.choices[j];
+              this.choices[j] = temp;
+            }
+          }
      }
-}
