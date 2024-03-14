da783240973357a8ba453333762f061773eda15f
Michael Nguyen <michael.nguyen@live.nl>
2024-03-14 21:16:10 +0100

    class quiz done

--- src/question.js
+++ src/question.js
@@ -4,10 +4,11 @@ class Question {
         this.choices = choices;
         this.answer = answer;
         this.difficulty = difficulty;
+
     }
 
      shuffleChoices(){
-        
+
             for (let i = this.choices.length - 1; i > 0; i--) {
               const j = Math.floor(Math.random() * (i + 1));
               const temp = this.choices[i];
--- src/quiz.js
+++ src/quiz.js
@@ -1,9 +1,16 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  
+  constructor (questions, timeLimit, timeRemaining){
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
+ getQuestion(){
+    
+ }
     
     // 3. moveToNextQuestion()
 
