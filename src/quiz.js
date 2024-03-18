bdde2234a1d0137cfdf3ccb1c3cdf5559510a0dd
tiagosarmentonogueira <tiagosarnog@gmail.com>
2024-03-18 16:35:38 +0000

    Co-authored-by: VinothaArivalagan <VinothaArivalagan@users.noreply.github.com>

--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,49 @@
 class Quiz {
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = this.correctAnswers
+        this.currentQuestionIndex = this.currentQuestionIndex
+        
+    }
+        correctAnswers = 0
+        currentQuestionIndex = 0
+    
+        getQuestion() {
+        return this.questions[this.currentQuestionIndex];   
+        }
+
+        moveToNextQuestion(){
+           this.currentQuestionIndex +=1;
+        }
+
+        shuffleQuestions(){
+            for (let i = this.questions.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+            }
+        }
+        
+        
+
+        checkAnswer(answer){
+            this.checkAnswer = this.checkAnswer;
+                this.correctAnswers +=1;
+            }
+
+        hasEnded(){
+         if(this.currentQuestionIndex < this.questions.length){
+            return false;
+         }
+         else if (this.currentQuestionIndex = this.questions.length){ 
+            return true;}
+        }    
+            
+        }
+       
+    
+
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
@@ -12,4 +57,3 @@ class Quiz {
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-}
